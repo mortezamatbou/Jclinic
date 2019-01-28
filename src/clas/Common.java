fe3f9bdd8952e86aefbe029a5c5a7e85d2ee37d4
Morteza Matbou <mortezamatbou91@gmail.com>
2019-01-29 01:21:17 +0330

    add methods for convert day number to day name and conversely

--- src/clas/Common.java
+++ src/clas/Common.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package clas;
 
 /**
@@ -11,5 +10,43 @@ package clas;
  * @author Lobdown
  */
 public class Common {
+
+    public static String getDayName(int dayIndex) {
+        switch (dayIndex) {
+            case 1:
+                return "شنبه";
+            case 2:
+                return "یکشنبه";
+            case 3:
+                return "دوشنبه";
+            case 4:
+                return "سه شنبه";
+            case 5:
+                return "چهارشنبه";
+            case 6:
+                return "پنج شنبه";
+            default:
+                return "نا معلوم";
+        }
+    }
     
+    public static int getDayIndex(String dayName) {
+        switch (dayName) {
+            case "شنبه":
+                return 1;
+            case "یکشنبه":
+                return 2;
+            case "دوشنبه":
+                return 3;
+            case "سه شنبه":
+                return 4;
+            case "چهارشنبه":
+                return 5;
+            case "پنج شنبه":
+                return 6;
+            default:
+                return 0;
+        }
+    }
+
 }
