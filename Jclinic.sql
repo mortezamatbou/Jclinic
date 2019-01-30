c1f366f028709890d95557e2aa6db38219aaa285
Morteza Matbou <mortezamatbou91@gmail.com>
2019-01-30 04:01:19 +0330

    add sql file for making database

---
+++ Jclinic.sql
@@ -0,0 +1,122 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 30, 2019 at 01:26 AM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sepid`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_name` varchar(50) COLLATE utf8_persian_ci NOT NULL,
+  `admin_username` varchar(50) COLLATE utf8_persian_ci NOT NULL,
+  `admin_password` varchar(32) COLLATE utf8_persian_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `admin_name`, `admin_username`, `admin_password`) VALUES
+(11253782, 'کاربر مدیر', 'admin', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_fname` varchar(50) COLLATE utf8_persian_ci NOT NULL,
+  `user_lname` varchar(50) COLLATE utf8_persian_ci NOT NULL,
+  `user_day` smallint(6) NOT NULL DEFAULT '1'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visit`
+--
+
+CREATE TABLE `visit` (
+  `visit_id` bigint(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `visit_date` date NOT NULL,
+  `day` smallint(6) NOT NULL,
+  `next_visit` date NOT NULL,
+  `ghors_5` varchar(6) COLLATE utf8_bin DEFAULT '0',
+  `ghors_20` varchar(6) COLLATE utf8_bin DEFAULT '0',
+  `ghors_40` varchar(6) COLLATE utf8_bin DEFAULT '0',
+  `metadon` varchar(6) COLLATE utf8_bin DEFAULT '0',
+  `bupre_4` varchar(6) COLLATE utf8_bin DEFAULT '0',
+  `bupre_2` varchar(6) COLLATE utf8_bin DEFAULT '0',
+  `bupre_8` varchar(6) COLLATE utf8_bin DEFAULT '0',
+  `bupre_bn` varchar(6) COLLATE utf8_bin DEFAULT '0',
+  `opiom` varchar(6) COLLATE utf8_bin DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `visit`
+--
+ALTER TABLE `visit`
+  ADD PRIMARY KEY (`visit_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11253783;
+
+--
+-- AUTO_INCREMENT for table `visit`
+--
+ALTER TABLE `visit`
+  MODIFY `visit_id` bigint(20) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
