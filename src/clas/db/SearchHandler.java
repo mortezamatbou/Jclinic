a4e4b628eda5b6f27cad5a5435511a848c39ba69
Morteza Matbou <mortezamatbou91@gmail.com>
2019-02-06 01:35:53 +0330

    add getUserDay for return user day visiting

--- src/clas/db/SearchHandler.java
+++ src/clas/db/SearchHandler.java
@@ -1,5 +1,6 @@
 package clas.db;
 
+import clas.Common;
 import java.sql.ResultSet;
 
 /**
@@ -20,11 +21,12 @@ public class SearchHandler extends DatabaseHandler {
             return null;
         }
     }
+
     public String getUserName(int id) {
         try {
             resultSet = statement.executeQuery("SELECT * FROM user WHERE user_id='" + id + "'");
             if (resultSet.next()) {
-                
+
                 return resultSet.getObject(2) + " " + resultSet.getObject(3);
             }
             return "نا مشخص";
@@ -71,6 +73,7 @@ public class SearchHandler extends DatabaseHandler {
             return null;
         }
     }
+
     public ResultSet searchOneDayUser(String userId, String date) {
         try {
             resultSet = statement.executeQuery("SELECT * FROM visit WHERE visit_date = '" + date + "' AND user_id = '" + userId + "' ORDER BY visit_date ASC");
@@ -83,6 +86,7 @@ public class SearchHandler extends DatabaseHandler {
             return null;
         }
     }
+
     public ResultSet searchBetween(String start, String end) {
         try {
             resultSet = statement.executeQuery("SELECT * FROM visit WHERE visit_date between '" + start + "' AND '" + end + "' ORDER BY visit_date ASC LIMIT 50");
@@ -95,6 +99,7 @@ public class SearchHandler extends DatabaseHandler {
             return null;
         }
     }
+
     public ResultSet searchBetweenPage(String start, String end, int page) {
         try {
             resultSet = statement.executeQuery("SELECT * FROM visit WHERE visit_date between '" + start + "' AND '" + end + "' ORDER BY visit_date ASC LIMIT 50");
@@ -107,6 +112,7 @@ public class SearchHandler extends DatabaseHandler {
             return null;
         }
     }
+
     public ResultSet searchBetween(String userId, String start, String end) {
         try {
             resultSet = statement.executeQuery("SELECT * FROM visit WHERE visit_date between '" + start + "' AND '" + end + "' AND user_id = '" + userId + "' ORDER BY visit_date ASC LIMIT 50");
@@ -119,7 +125,7 @@ public class SearchHandler extends DatabaseHandler {
             return null;
         }
     }
-    
+
     public ResultSet searchVisitDate(String date) {
         try {
             resultSet = statement.executeQuery("SELECT * FROM visit WHERE next_visit = '" + date + "'");
@@ -132,7 +138,7 @@ public class SearchHandler extends DatabaseHandler {
             return null;
         }
     }
-    
+
     public ResultSet searchBetweenPage(String userId, String start, String end, int page) {
         try {
             resultSet = statement.executeQuery("SELECT * FROM visit WHERE visit_date between '" + start + "' AND '" + end + "' AND user_id = '" + userId + "' ORDER BY visit_date ASC LIMIT 50");
@@ -144,6 +150,18 @@ public class SearchHandler extends DatabaseHandler {
         } catch (Exception e) {
             return null;
         }
+    }
+
+    public String getUserDay(String userId) {
+        try {
+            resultSet = statement.executeQuery("SELECT user_day FROM user WHERE user_id = '" + userId + "'");
+            if (resultSet.next()) {
+                return Common.getDayName(Integer.parseInt(resultSet.getString(1)));
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return null;
     }
 
 }
